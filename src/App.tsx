56539eedbf240177423ec04e30c17b4e57862d0d
Muhammed Fatih Candan <mfatihcandan@gmail.com>
2021-11-18 12:34:43 +0300

    data mapping

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import { useQuery } from "react-query";
 //components
+import Item from "./Item/Item";
 import { Drawer } from "@material-ui/core";
-import { LinearProgress } from "@material-ui/core/";
+import { LinearProgress } from "@material-ui/core";
 import { Grid } from "@material-ui/core";
 import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
 import { Badge } from "@material-ui/core";
@@ -29,7 +30,27 @@ const App = () => {
     getProducts
   );
   console.log(data);
-  return <div className="App">Hello TS</div>;
+
+  const getTotalItems = () => null;
+
+  const handleAddToCart = () => null;
+
+  const handleRemoveFromCart = (clickedItem: CartItemType) => null;
+
+  if (isLoading) return <LinearProgress />;
+  if (error) return <div>Someting went wrong...</div>;
+
+  return (
+    <Wrapper>
+      <Grid container spacing={3}>
+        {data?.map((item) => (
+          <Grid item key={item.id} xs={12} sm={4}>
+            <Item item={item} handleAddToCart={handleAddToCart} />
+          </Grid>
+        ))}
+      </Grid>
+    </Wrapper>
+  );
 };
 
 export default App;
